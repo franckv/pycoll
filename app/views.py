49e78958809e6fbcddd45b75ba0b3eb4962ef4ec
franck <franck@Yggdrasil.localdomain>
2009-04-13 17:17:49 +0200

    Merge item_edit and item_post. Merge item_add and item_save.

--- app/views.py
+++ app/views.py
@@ -39,7 +39,23 @@ def item(request, item_id):
 
 def item_edit(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
-    return render_to_response('app/item_edit.html', {'item': item})
+
+    if request.method == 'POST':
+	try:
+	    if len(request.POST['name']) == 0:
+		return render_to_response('app/item_edit.html', {'item': item})
+	    type = get_object_or_404(ItemType, name=request.POST['type'])
+	    item.name = request.POST['name']
+	    item.type = type
+	    item.description = request.POST['description']
+	    item.update_date = datetime.datetime.now()
+	except (KeyError):
+	    return render_to_response('app/item_edit.html', {'item': item})
+	else:
+	    item.save()
+	return HttpResponseRedirect(item.get_absolute_url())
+    else:
+	return render_to_response('app/item_edit.html', {'item': item})
 
 def item_delete(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
@@ -50,49 +66,33 @@ def item_delete_force(request, item_id):
     item.delete()
     return HttpResponseRedirect(reverse('app.views.items'))
 
-def item_post(request, item_id):
-    item = get_object_or_404(Item, pk=item_id)
-    try:
-	if len(request.POST['name']) == 0:
-	    return render_to_response('app/item_edit.html', {'item': item})
-	type = get_object_or_404(ItemType, name=request.POST['type'])
-	item.name = request.POST['name']
-	item.type = type
-	item.description = request.POST['description']
-	item.update_date = datetime.datetime.now()
-    except (KeyError):
-	return render_to_response('app/item_edit.html', {'item': item})
-    else:
-	item.save()
-	return HttpResponseRedirect(item.get_absolute_url())
-
 def item_add(request):
-    type = get_object_or_404(ItemType, name='DVD')
-    return render_to_response('app/item_add.html', {'type': type})
+    if request.method == 'POST':
+	try:
+	    itemtype = get_object_or_404(ItemType, name=request.POST['type'])
+	    if itemtype.name == 'CD':
+		item = CD()
+		item.type = itemtype
+	    elif itemtype.name == 'DVD':
+		item = DVD()
+		item.type = itemtype
+	    else:
+		item = Item()
 
-def item_save(request):
-    try:
-	itemtype = get_object_or_404(ItemType, name=request.POST['type'])
-	if itemtype.name == 'CD':
-	    item = CD()
-	    item.type = itemtype
-	elif itemtype.name == 'DVD':
-	    item = DVD()
-	    item.type = itemtype
-	else:
-	    item = Item()
-
-	if len(request.POST['name']) == 0:
+	    if len(request.POST['name']) == 0:
+		return render_to_response('app/item_add.html')
+	    item.name = request.POST['name']
+	    item.description=request.POST['description']
+	    item.creation_date = datetime.datetime.now()
+	    item.update_date = datetime.datetime.now()
+	except (KeyError):
 	    return render_to_response('app/item_add.html')
-	item.name = request.POST['name']
-	item.description=request.POST['description']
-	item.creation_date = datetime.datetime.now()
-	item.update_date = datetime.datetime.now()
-    except (KeyError):
-	return render_to_response('app/item_add.html')
+	else:
+	    item.save()
+	    return HttpResponseRedirect(item.get_absolute_url())
     else:
-	item.save()
-	return HttpResponseRedirect(item.get_absolute_url())
+	type = get_object_or_404(ItemType, name='DVD')
+	return render_to_response('app/item_add.html', {'type': type})
 
 def category(request, type_id):
     item_list = Item.objects.filter(type=type_id)
